e22e8c69a9302f5497c981a79501b88d3e0cee94
Naimanshalov Ilias <mail@nabster.name>
2025-02-24 03:40:25 +0500

    meta tag

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,8 @@ import {fetchData} from "@/lib/api";
 import {StoryblokStory} from "@storyblok/react/rsc";
 import {StoryblokComponent} from "@storyblok/react";
 
+
+
 export default async function Home() {
   const { data } = await fetchData("home", {
     version: "draft",
